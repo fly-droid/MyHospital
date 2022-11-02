84cf8e2887ce3fea765c64b0874aba5ffb62aab8
water <21009258@mvula.univen.ac.za>
2022-11-02 14:23:11 +0200

    added the functionality of the go back text and register question text

--- app/src/main/java/com/example/myhospital/MainActivity.java
+++ app/src/main/java/com/example/myhospital/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.myhospital;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView loginQuestion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginQuestion = findViewById(R.id.loginQuestion);
+
+        loginQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentReg = new Intent(  MainActivity .this, Registration.class);
+                startActivity(intentReg);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myhospital/Registration.java
+++ app/src/main/java/com/example/myhospital/Registration.java
@@ -1,13 +1,27 @@
 package com.example.myhospital;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Registration extends AppCompatActivity {
-
+    private TextView Goback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration);
+
+        Goback = findViewById(R.id.tvbacklog);
+
+        Goback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLog = new Intent( Registration .this, MainActivity.class);
+                startActivity(intentLog);
+            }
+        });
     }
 }
