cc4b2336b8f649091376be8bff42b00a4632bac8
water <21009258@mvula.univen.ac.za>
2022-11-04 12:40:52 +0200

    Connected to firebase and add a Doctor added profile

--- app/src/main/java/com/example/myhospital/MainActivity.java
+++ app/src/main/java/com/example/myhospital/MainActivity.java
@@ -1,21 +1,82 @@
 package com.example.myhospital;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editTextLoginEmail, editTextLoginPwd;
+    private ProgressBar progressBar;
+    private FirebaseAuth authProfile;
     private TextView loginQuestion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        loginQuestion = findViewById(R.id.loginQuestion);
+        getSupportActionBar().setTitle("Login");
 
+        editTextLoginEmail = findViewById(R.id.loginEmail);
+        editTextLoginPwd = findViewById(R.id.loginPassword);
+        progressBar = findViewById(R.id.progress_bar);
+        loginQuestion = findViewById(R.id.loginQuestion);
+        authProfile =FirebaseAuth.getInstance();
+        Button loginButton =  findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String textEmail = editTextLoginEmail.getText().toString();
+                String textPassword = editTextLoginPwd.getText().toString();
+
+                if(TextUtils.isEmpty(textEmail)){
+                    Toast.makeText(MainActivity.this, "Please enter your email", Toast.LENGTH_SHORT).show();
+                    editTextLoginEmail.setError("Email is required");
+                    editTextLoginEmail.requestFocus();
+                }else if(!Patterns.EMAIL_ADDRESS.matcher(textEmail).matches()){
+                    Toast.makeText(MainActivity.this, "Please re-enter your email", Toast.LENGTH_SHORT).show();
+                    editTextLoginEmail.setError("Incorrect emal format");
+                    editTextLoginEmail.requestFocus();
+                }else if(TextUtils.isEmpty(textPassword)){
+                    Toast.makeText(MainActivity.this, "Please enter your password", Toast.LENGTH_SHORT).show();
+                    editTextLoginPwd.setError("Password is required");
+                    editTextLoginPwd.requestFocus();
+                }else{
+                    progressBar.setVisibility(View.VISIBLE);
+                    loginUser(textEmail,textPassword);
+                }
+                Intent intentProf = new Intent(  MainActivity .this, PatientProfile.class);
+                startActivity(intentProf);
+            }                                                 
+
+            private void loginUser(String email, String password) {
+                 authProfile.signInWithEmailAndPassword(email,password).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                     @Override
+                     public void onComplete(@NonNull Task<AuthResult> task) {
+                           if(task.isSuccessful()) {
+                               Toast.makeText(MainActivity.this, "You are login successfully", Toast.LENGTH_SHORT).show();
+                           }else{
+                               Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                           }
+                           progressBar.setVisibility(View.GONE);
+                     }
+                 });
+            }
+        });
         loginQuestion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/myhospital/PatientProfile.java
+++ app/src/main/java/com/example/myhospital/PatientProfile.java
@@ -2,12 +2,21 @@ package com.example.myhospital;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class PatientProfile extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_profile);
+
+
+
+
     }
 }
